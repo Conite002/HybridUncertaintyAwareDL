aba3d1df4918e91ebf6ac45edddf3088317beb5f
Conite002 <conite.gbodogbe@imsp-uac.org>
2025-05-28 10:50:48 +0100

    Add new ResNet initialization functions for ovarian dataset; update training results in JSON files

--- src/models/resnet_model.py
+++ src/models/resnet_model.py
@@ -1,7 +1,7 @@
 import torch.nn as nn
 import torchvision.models as models
 
-def initialize_resnet(num_classes, dropout_rate=0.5, mc_dropout=False, device='cuda'):
+def initialize_resnet_sipakmed(num_classes, dropout_rate=0.5, mc_dropout=False, device='cuda'):
     model = models.resnet50(pretrained=True)
     
     for param in model.parameters():
@@ -24,4 +24,33 @@ def initialize_resnet(num_classes, dropout_rate=0.5, mc_dropout=False, device='c
     )
     
     return model.to(device)
+
+
+
+
+
+def initialize_resnet_ovarian(num_classes, dropout_rate=0.5, mc_dropout=False, device='cuda'):
+    model = models.resnet50(pretrained=True)
+    
+    for param in model.parameters():
+        param.requires_grad = False
+        
+    for name, param in model.named_parameters():
+        if "layer3" in name or "layer4" in name:
+            param.requires_grad = True
+        in_features = model.fc.in_features
+        # Define new classifier with MC Dropout if enabled
+        dropout_layer = nn.Dropout(p=dropout_rate) if mc_dropout else nn.Identity()
+        model.classifier = nn.Sequential(
+            nn.Linear(in_features, 1024),
+            nn.ReLU(),
+            dropout_layer,
+            nn.Linear(1024, 128),
+            nn.ReLU(),
+            # dropout_layer,
+            nn.Linear(128, 5)
+        )
+    
+    return model.to(device)
+
 
